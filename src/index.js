023a8f70fc62fb5aa4476167d3b51e80cfb83d00
SergeiOsmolovskii <36640948+SergeiOsmolovskii@users.noreply.github.com>
2018-02-21 20:15:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 class Sorter {
   constructor() {
     // your implementation
+    this.massiv=[];
+
   }
 
   add(element) {
     // your implementation
+    this.massiv.push(element);
   }
 
   at(index) {
     // your implementation
+    return this.massiv[index];
   }
 
   get length() {
     // your implementation
+    return this.massiv.length;
   }
 
   toArray() {
     // your implementation
+    return this.massiv;
   }
 
   sort(indices) {
     // your implementation
+    this.massiv.sort();
   }
 
   setComparator(compareFunction) {
     // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
