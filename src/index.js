207a7a2aa0fdd1215f81e397efff34d36f9f5ca1
SergeiOsmolovskii <36640948+SergeiOsmolovskii@users.noreply.github.com>
2018-02-22 15:37:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 class Sorter {
-  constructor() {
+  constructor(sortMethod) {
     // your implementation
     this.massiv=[];
-
+   this.compareFunction = null;
   }
 
   add(element) {
@@ -23,17 +23,34 @@ class Sorter {
   toArray() {
     // your implementation
     return this.massiv;
+   
   }
 
-  sort(indices) {
+  sort(indices) 
+{
     // your implementation
-    this.massiv.sort();
-  }
+     this.massiv2=[];
+      indices = indices.sort(function(a,b){
+         return a-b
+      });
+       for(var i=0; i<indices.length; i++){
+         this.massiv2[i]=this.massiv[indices[i]];
+      };
+       if(this.compareFunction == null){
+         this.massiv2.sort(function(a,b){
+           return a-b
+      });
+      }
+       else this.massiv2.sort(this.compareFunction);
+       for(var i = 0; i <this.massiv2.length; i++ ) {
+         this.massiv[indices[i]] = this.massiv2[i];
+      };
+       return this.massiv;
+}
 
   setComparator(compareFunction) {
     // your implementation
-    this.compareFunction = compareFunction;
+     this.compareFunction = compareFunction;   
   }
 }
-
 module.exports = Sorter;
